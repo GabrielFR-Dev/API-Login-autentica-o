082a58b97f30e436983030052860e1712dd676ac
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-05-30 11:18:10 -0300

    testando a conexao com o banco de dados

---
+++ API-Autenticacao/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import pool from './servico/conexao.js';
+
+
+const app = express();
+app.use(cors());
+
+
+app.listen(3001, async() => {
+    const data = new Date();
+    
+    const conexao = await pool.getConnection();
+
+    console.log(`Servidor iniciado em ${data}`);
+    console.log(conexao.threadId);
+
+    conexao.release();
+})
--- API-Autenticacao/servico/conexao.js
+++ API-Autenticacao/servico/conexao.js
@@ -4,8 +4,10 @@ import env from 'dotenv';
 env.config();
 
 const pool = mysql.createPool({
-    host:  
-    user:   
-    password:   
-    database:
+    host: process.env.LS_HOST,
+    user: process.env.LS_USER,
+    password: process.env.LS_PASSWORD,
+    database: process.env.LS_DATABASE
 })
+
+export default pool;
